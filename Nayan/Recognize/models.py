165caf6fb400c9630e61f6ddfc7b1b6776200dbd
SASIN83 <nisarsasin123@gmail.com>
2021-05-30 19:26:08 +0530

    Merged all changes made

--- Nayan/Recognize/models.py
+++ Nayan/Recognize/models.py
@@ -2,29 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 import datetime
-'''
-
-# Create your models here.
-class Category(models.Model):
-    title = models.CharField(max_length=25)
-    slug = models.SlugField(null=False, unique=True)
-
-    def get_absolute_url(self):
-        return reverse('categories', args=[self.slug])
-
-    def __str__(self):
-        return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.title.replace(" ", "")
-            self.slug = slugify(self.title)
-        return super().save(*args, **kwargs)
-
-    class Meta:
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
-'''
 
 CATEGORY_CHOICES = (
     ('miss','MISSINGS'),
@@ -43,17 +20,7 @@ class ImageUploader(models.Model):
     photo = models.ImageField(upload_to="images")
     date = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=7, choices=CATEGORY_CHOICES, default='home')
-<<<<<<< HEAD
-    filter = models.CharField(max_length=7, choices=FILTER_CHOICES)
-=======
     filter = models.CharField(max_length=7, choices=FILTER_CHOICES, default='all')
->>>>>>> 1cffb409ceaeed1f61c8d9128112c2aeb1e1895c
-
-    '''
-    def __str__(self):
-        return str(self.id)
-    '''
-
     def save(self, *args, **kwargs):
         self.title = self.photo.name
         self.date = datetime.datetime.now()
